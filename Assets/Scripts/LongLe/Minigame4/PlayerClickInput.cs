a9ca0a690cece0e83eed6df48953dab56bcec320
48027480 <levietphilong2004@gmail.com>
2026-07-25 18:09:07 +1000

    commit

---
+++ Assets/Scripts/LongLe/Minigame4/PlayerClickInput.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerClickInput : MonoBehaviour
+{
+    [SerializeField] private Camera mainCamera;
+
+    private void Awake()
+    {
+        if (mainCamera == null)
+        {
+            mainCamera = Camera.main;
+        }
+    }
+
+    private void Update()
+    {
+        // Check for click/tap using New Input System
+        if (Pointer.current != null && Pointer.current.press.wasPressedThisFrame)
+        {
+            Vector2 mousePosition = Pointer.current.position.ReadValue();
+            Vector2 worldPosition = mainCamera.ScreenToWorldPoint(mousePosition);
+
+            RaycastHit2D hit = Physics2D.Raycast(worldPosition, Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                // Trigger click on item if found
+                hit.collider.SendMessage("OnMouseDown", SendMessageOptions.DontRequireReceiver);
+            }
+        }
+    }
+}
